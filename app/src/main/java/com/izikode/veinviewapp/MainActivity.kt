ecdeaea1cd2daf933e6322824e2ce807eee64963
ATH\fanveizi <fanis.veizi@intrasoft-intl.com>
2018-05-24 15:57:03 +0300

    init

--- app/src/main/java/com/izikode/veinviewapp/MainActivity.kt
+++ app/src/main/java/com/izikode/veinviewapp/MainActivity.kt
@@ -5,11 +5,7 @@ import android.os.Bundle
 import android.webkit.WebView
 import com.izikode.veinview.VeinView
 import com.izikode.veinview.VeinViewClient
-import com.izikode.veinview.VeinViewClientCallbacks
 import com.izikode.veinview.VeinViewInjector
-import android.webkit.WebSettings
-
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +33,6 @@ class MainActivity : AppCompatActivity() {
         settings.setLoadWithOverviewMode(true)
         settings.setUseWideViewPort(true)
 
-        veinView.loadUrl("https://secure.mobile.pollachilena.performgroup.com/streaming/watch/event/index.html?userId=0&partnerId=8205&eventId=5r38zfrlpi1ulcmqgt7oq4foq&flash=y")
+        veinView.loadUrl("https://www.google.com/")
     }
 }
